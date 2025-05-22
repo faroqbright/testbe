ed79fdfdf9afde8adfad45920970246c8adb999e
muhammad-ali-hassan-alvi <muhammadalialvi646@gmail.com>
2025-05-22 18:19:26 +0500

    Changes in Script

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const port = process.env.PORT || 5000;
 // ✅ Middlewares
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    origin: "*",
     credentials: true,
   })
 );
